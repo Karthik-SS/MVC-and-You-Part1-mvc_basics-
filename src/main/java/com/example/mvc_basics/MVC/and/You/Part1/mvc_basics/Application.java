77953be6636e97af574dedcd5280f4bedd58fd26
Karthik-SS <Karthieus@gmail.com>
2020-08-10 20:24:31 -0700

    Initial Commit For Review

---
+++ src/main/java/com/example/mvc_basics/MVC/and/You/Part1/mvc_basics/Application.java
@@ -0,0 +1,13 @@
+package com.example.mvc_basics.MVC.and.You.Part1.mvc_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvc_basics/MVC/and/You/Part1/mvc_basics/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc_basics.MVC.and.You.Part1.mvc_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
